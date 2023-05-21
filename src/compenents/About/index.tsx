5ea66035cfe0adcae0fd6bf235c0202464a25614
asadulladev <usmanbeliever@gmail.com>
2023-05-21 14:47:28 +0600

    marsel

---
+++ src/compenents/About/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div className="h-[500px] bg-amber-400 w-auto">
+                <h1 className="text-center text-6xl text-white">ABOUT</h1>
+
+            </div>
+        </div>
+    );
+};
+
+export default About;
