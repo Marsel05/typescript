68b22941bb018f71f9984e72fbed10dcaf4290f0
asadulladev <usmanbeliever@gmail.com>
2023-06-25 10:05:24 +0600

    react-type

--- src/compenents/About/index.tsx
+++ src/compenents/About/index.tsx
@@ -5,7 +5,6 @@ const About = () => {
         <div>
             <div className="h-[500px] bg-amber-400 w-auto">
                 <h1 className="text-center text-6xl text-white">ABOUT</h1>
-
             </div>
         </div>
     );
